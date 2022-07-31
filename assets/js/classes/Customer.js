cd29b8793734335e9dbdfb56d2b23b4f1835aaf3
April Lee <april210608@gmail.com>
2022-07-30 22:15:32 -0700

    signIn part and modal done

--- assets/js/classes/Customer.js
+++ assets/js/classes/Customer.js
@@ -3,7 +3,7 @@
  * @author Jose Luis CG
  * @version 1.0
  */
-class Customer extends User {
+ class Customer extends User {
     /**
      * @param userID {number} User ID
      * @param firstName {String} User First Name
@@ -13,8 +13,36 @@ class Customer extends User {
      * @param address {String} User Address Ej: 500 Pacific Street, Vancouver, BC, V67 683
      * @param email {String} User email
      * @param phoneNumber {String} User phone number
+     * @param creditCard {String} User credit Card
+     * @param expiryDate {String} User credit Card expiry date
      */
-    constructor(userID, firstName, lastName, bornDate, country, address, email, phoneNumber) {
-        super(userID, firstName, lastName, bornDate, country, address, email, phoneNumber);
+    constructor(userID, firstName, lastName, bornDate, country, address, email, phoneNumber, creditCard, expiryDate) {
+        super(userID, firstName, lastName, bornDate, country, address, email, phoneNumber, creditCard, expiryDate);
+    }
+
+    //METHODS
+    /** Returns
+     * This method get customer age
+     * @return {number}
+     */
+    getAge() {
+        let today = new Date();
+        let age = 0;
+        let month = 0;
+        age = today.getFullYear() - this.bornDate.split('/')[2];
+        month = today.getMonth() - this.bornDate.split('/')[0];
+
+        if (month < 0 || (month === 0 && today.getDate() < this.bornDate.split('/')[1])) {
+            age--;
+        }
+        return age;
+    }
+
+    /** Returns
+     * This method check if the customer is bigger than 18
+     * @return {boolean}
+     */
+    isOlder() {
+        return this.getAge() >= 19;
     }
 }
---
+++ assets/js/classes/js_functions/modal.js
@@ -0,0 +1,51 @@
+// set cookie to remember when user click Yes or No
+let setMainCookie = (CookieName, CookieValue, Exp) => {
+  const currentTime = new Date();
+  currentTime.setTime(currentTime.getTime() + Exp * 24 * 60 * 60 * 1000);
+  let Cookie_Exp = "expires=" + currentTime.toUTCString();
+  document.cookie = " " + CookieName + "=" + CookieValue + ";" + Cookie_Exp;
+};
+
+// get cookie name 
+let getMainCookie = (CookieName) => {
+  let cookieKey = CookieName + "=";
+  let cookies = decodeURIComponent(document.cookie);
+  let cookieArray = cookies.split(";");
+  for (let cookie of cookieArray) {
+    while (cookie.charAt(0) == " ") {
+      cookie = cookie.substring(1);
+    }
+    if (cookie.indexOf(cookieKey) == 0) {
+      return cookie.substring(cookieKey.length);
+    }
+  }
+  return "";
+};
+
+// alert message if user never visited before
+if (getMainCookie("isAdult") === "") {
+  swal("Are you over 19 years old ?", {
+    buttons: {
+      cancel: "No",
+      catch: {
+        text: "Yes",
+        value: "catch",
+      },
+    },
+  })
+    .then((value) => {
+      switch (value) {
+
+        case "catch":
+          setMainCookie("isAdult", 'Y', 4);
+          swal("Enjoy Shopping!", "", "success");
+          break;
+
+        default:
+          setMainCookie("isAdult", 'N', 4);
+          window.location.href = "https://google.com";
+          break;
+      }
+    });
+}
+
